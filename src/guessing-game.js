1b24634cd78456324c81b52f39eda8d2a2db976f
Anton <samusenkoanton@yandex.ru>
2019-10-13 02:21:20 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = 0;
+        this.end = 0;
+        this.a = 0;
+    }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.a = Math.round((this.end  + this.start)/2);
+        return this.a;
     }
 
     lower() {
-
+        this.end = this.a;
     }
 
     greater() {
-
+        this.start = this.a;
     }
 }
 
